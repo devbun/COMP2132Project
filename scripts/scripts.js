76b529ff5803fd0cdf4986109c979c02b0a85509
devbun <conanaleong@gmail.com>
2023-11-24 17:05:07 -0800

    v1.1 added more words

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,24 @@
 
 var theword = [];
 
+var guesses = [];
+
 var gameover = false;
 
-const MISTAKE_LIMIT = 7
+const MISTAKE_LIMIT = 7;
 
 var WORDS = {
     "SKELETON": "There's one inside you right now",
-    "JAVASCRIPT": "It's not as good as Python (kidding)",
-    "FISH": "glub glub"
+    "BEYONCE": "Should have won album of the year",
+    "BANANAS": "Yellow snack, vehiclar destruction",
+    "ELEPHANT": "Big ears, long nose, never forgets",
+    "COMPUTER": "It kind of ruined everything didn't it",
+    "MONKEYS": "Like us but more fun",
+    "BASEBALL": "Bat, ball, and a seventh-inning stretch.",
+    "DIAMOND": "Just buy a fake one",
+    "ELEVATE": "Lift yourself up",
+    "JUGGLING": "When you have no one to play catch with."
+    
 }
 
 function NewGame() {
@@ -31,24 +41,7 @@ function SelectLetter(letter) {
     console.log(guesses)
     UpdateDisplay()
 
-    let right = 0;
-    let wrong = 0;
-    for (let i = 0; i < guesses.length; i++) {
-        if ([...theword].includes(guesses[i])) {
-            right++
-        } else {
-            wrong ++
-        }
-      }
-
-      console.log('right:' + right)
-      console.log('wrong' + wrong)
-
-      const uniques = new Set([...theword])
-
-      if (right >= uniques.size) GameEnd('win')
-      if (wrong >= MISTAKE_LIMIT ) GameEnd('lose')
-
+    //Could combine this with update display?
 }
 
 function UpdateDisplay() {
@@ -66,7 +59,7 @@ for (let i = 0; i < guesses.length; i++) {
 
   if (wrong > MISTAKE_LIMIT) wrong = MISTAKE_LIMIT;
 
-  document.getElementById("hamgmanimg").src = "images/hangman" + wrong + ".png"
+  document.getElementById("hamgmanimg").src = "images/hangman" + wrong + ".png" //template literal is better?
 
 
 //guesses
@@ -76,13 +69,17 @@ letterbox.innerHTML = '';
 for (const letter of alphabet) {
     const displayLetter = document.createElement('div');
     displayLetter.classList.add('letter');
-    displayLetter.classList.add('selectable');
     displayLetter.addEventListener('click', function() {
         SelectLetter(letter)
         
     });
 
-    if (guesses.includes(letter)) displayLetter.classList.add('guessed');
+    if (guesses.includes(letter)) {
+        displayLetter.classList.add('guessed')
+    } else {
+            displayLetter.classList.add('selectable')
+        }
+
     displayLetter.textContent = letter;
     letterbox.appendChild(displayLetter)
   }
@@ -96,6 +93,12 @@ for (const letter of theword) {
     displayLetter.textContent = (guesses.includes(letter)) ? letter : "_"
     wordbox.appendChild(displayLetter)
   }
+
+  const uniques = new Set([...theword])
+
+  if (right >= uniques.size) GameEnd('win')
+  if (wrong >= MISTAKE_LIMIT ) GameEnd('lose')
+
 }
 
 function GameEnd(result) {
